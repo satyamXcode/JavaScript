1838349efafa2dc5a57c76a676efe18691a419b0
satyamXcode <jaiswalsatyam876@gmail.com>
2025-05-23 03:53:06 +0530

    Understand: Data Types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,15 @@
+
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => undefined(Not assigned any value)
+// symbol => nnique
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
