fe93bdfe05813701f2309888da96027b85a2af40
satyamXcode <jaiswalsatyam876@gmail.com>
2025-06-05 22:32:40 +0530

    Understanding: Date Methods

---
+++ 01_basics/07_date.js
@@ -0,0 +1,13 @@
+const myDate = new Date();
+
+// console.log(myDate);
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toISOString());
+// console.log(typeof(myDate));
+
+// let myCreatedDate = new Date(2023, 0, 23);
+// let myCreatedDate = new Date(2023, 0, 23, 5, 3);
+
+let myCreatedDate = new Date("01-14-2025");
+console.log(myCreatedDate.toLocaleString());
