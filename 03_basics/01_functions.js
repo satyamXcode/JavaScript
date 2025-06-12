c90995cb6dda31ff6d14e4dc8972e4132ba79631
satyamXcode <jaiswalsatyam876@gmail.com>
2025-06-12 22:33:59 +0530

    Understanding: Functions Concept

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,18 @@
+function addNumber01(number1, number2){
+console.log(number1+number2);
+}
+
+addNumber01(9,11);
+
+function addNumber02(num1, num2){
+    return (num1 + num2);
+}
+
+console.log(addNumber02(7, 8));
+
+
+function userLoggedIn(username){
+    return `${username} logged In`;
+}
+
+console.log(userLoggedIn("satyam"));
