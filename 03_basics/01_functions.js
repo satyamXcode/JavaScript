a321b7391dd930b489f94a257814a7117d3bb43b
satyamXcode <jaiswalsatyam876@gmail.com>
2025-06-12 22:55:21 +0530

    Understand: Rest Operator(Function)

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -1,18 +1,35 @@
-function addNumber01(number1, number2){
-console.log(number1+number2);
+// function addNumber01(number1, number2){
+// console.log(number1+number2);
+// }
+
+// addNumber01(9,11);
+
+// function addNumber02(num1, num2){
+//     return (num1 + num2);
+// }
+
+// console.log(addNumber02(7, 8));
+
+
+// function userLoggedIn(username){
+//     return `${username} logged In`;
+// }
+
+// console.log(userLoggedIn("satyam"));
+
+// function calculateCartPrice(...num1){  //rest operator
+//     return num1; 
+// }
+
+// console.log(calculateCartPrice(100, 200, 300, 400, 500));
+
+const user = {
+    userName: "Satyam Jaiswal",
+    status: "Offline"
 }
 
-addNumber01(9,11);
-
-function addNumber02(num1, num2){
-    return (num1 + num2);
+function userDetails(anyUser){
+  console.log(`${anyUser.userName} is showing ${user.status}`);
 }
 
-console.log(addNumber02(7, 8));
-
-
-function userLoggedIn(username){
-    return `${username} logged In`;
-}
-
-console.log(userLoggedIn("satyam"));
+userDetails(user);
